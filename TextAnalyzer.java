1fd7e224c32719a872bce8699fc013b3014794b8
howell <samcaldwell@utexas.edu>
2013-04-08 22:28:51 -0500

    first version

--- TextAnalyzer.java
+++ TextAnalyzer.java
@@ -12,10 +12,41 @@ public class TextAnalyzer {
     
     public static class Map extends MapReduceBase implements
     Mapper<LongWritable, Text, Text, IntWritable> {
+
+        protected String mContext = "Darcy";
+        protected String mQuery = "money";
     
+        // key is in the format "[contextWord] [queryWord]"
+        // value is the line of text
         public void map(LongWritable key, Text value, OutputCollector<Text,
         IntWritable> output, Reporter reporter) throws IOException {
+            // format the line of text
+            String line = value.toString().toLowerCase();
+            line = line.replaceAll("[^a-z]", "");
+            Text ouputKey = new Text("mContext " + "mQuery");
+            if(line.contains(mContext.toLowerCase())) {
+                // take into account that if the context equals the query,
+                // one of the counts needs to be discared
+                int count = (mContext.equalsIgnoreCase(mQuery)) ? -1 : 0;
+                for(String w : line.split("\\s+")) {
+                    if(w.equalsIgnoreCase(mQuery)) {
+                        count++; 
+                    }
+                }
+                outputCount = new IntWritable(count);
+            }
+            else {
+                outputCount = new IntWritable(0);
+            }
+            output.collect(outputKey, outputCount);
+        }
 
+        public void setContext(String c) {
+            mContext = c;
+        }
+
+        public void setQuery(String q) {
+            mQuery = q;
         }
 
     }
@@ -26,12 +57,34 @@ public class TextAnalyzer {
         public void reduce(Text key, Iterator<IntWritable> values,
         OutputCollector<Text, IntWritable> output, Reporter reporter) throws
         IOException {
-
+            // sum the values
+            int sum = 0;
+            while(values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
         }
 
     }
 
     public static void main(String[] args) {
+        JobConf conf = new JobConf(TextAnalyzer.class);
+        conf.setJobName("textanalyzer");
 
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+        conf.setReducerClass(Reduce.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputFormat.setOutputPath(conf, new
+        Path(args[1]));
+
+        JobClient.runJob(conf);
     }
 }
