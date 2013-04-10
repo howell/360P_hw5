55d12f16b5e8a2e4cf18a2ce7547ebf8a1e4eb1f
hadoop <hadoop@wireless-198-213-233-165.public.utexas.edu>
2013-04-10 15:41:11 -0500

    now working
    found and fixed a problem that was caused by the namenode and datanodes having
    different namespaceIDs. This seems to be caused from formatting the namenode,
    and was fixed by removing all of the files in /private/tmp/hadoop-{username}/dfs
    The run file now relies on HADOOP_PREFIX being set

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
-rm -rf output/
+rm -rf output/ gen/
+hadoop dfs -rmr output
+mkdir output
+mkdir gen
 #javac -classpath /usr/local/Cellar/hadoop/1.1.2/libexec/hadoop-core-1.1.2.jar -d gen/ TextAnalyzer.java 
-javac -classpath /Users/hadoop/hadoop-1.0.4/hadoop-core-1.0.4.jar -d gen/ TextAnalyzer.java
+#javac -classpath /Users/hadoop/hadoop-1.0.4/hadoop-core-1.0.4.jar -d gen/ TextAnalyzer.java
+javac -classpath $HADOOP_PREFIX/hadoop-core-1.0.4.jar -d gen/ TextAnalyzer.java
 jar -cvf TextAnalyzer.jar -C gen/ .
-/Users/hadoop/hadoop-1.0.4/bin/hadoop jar TextAnalyzer.jar TextAnalyzer input/ output/ $1 $2
+#/Users/hadoop/hadoop-1.0.4/bin/hadoop jar TextAnalyzer.jar TextAnalyzer ./input/ ./output/ $1 $2
+hadoop jar TextAnalyzer.jar TextAnalyzer ./input/ ./output/ $1 $2
+hadoop dfs -cat /user/hadoop/output/part-00000
