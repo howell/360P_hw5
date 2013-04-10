6927de9f61fc7855e581effe1de2da598ad6472d
hadoop <hadoop@145.dyn225.apogeenet.net>
2013-04-09 23:30:01 -0500

    working version of pseudo distr.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 rm -rf output/
-javac -classpath /usr/local/Cellar/hadoop/1.1.2/libexec/hadoop-core-1.1.2.jar -d gen/ TextAnalyzer.java 
+#javac -classpath /usr/local/Cellar/hadoop/1.1.2/libexec/hadoop-core-1.1.2.jar -d gen/ TextAnalyzer.java 
+javac -classpath /Users/hadoop/hadoop-1.0.4/hadoop-core-1.0.4.jar -d gen/ TextAnalyzer.java
 jar -cvf TextAnalyzer.jar -C gen/ .
-hadoop jar TextAnalyzer.jar TextAnalyzer input/ output/ $1 $2
+/Users/hadoop/hadoop-1.0.4/bin/hadoop jar TextAnalyzer.jar TextAnalyzer input/ output/ $1 $2
